2545a2b5a90101cfca926178690019e962dcd5ab
Fabian Oliva Lopez <153218776+FabulousFabStar@users.noreply.github.com>
2025-06-24 21:22:20 -0500

    fix: main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import './assets/main.css'
 import { createApp } from 'vue'
 import PrimeVue from 'primevue/config'
-import '@primevue/themes/aura-light-blue/theme.css';
 import 'primeflex/primeflex.css';
 import 'primeicons/primeicons.css';
 import App from './App.vue'
@@ -12,7 +11,7 @@ import router from './router';
 
 const app = createApp(App)
 
-app.use(PrimeVue, { ripple: true })
+app
     .use(ToastService)
     .component('pv-card', Card)
     .component('pv-button', Button)
