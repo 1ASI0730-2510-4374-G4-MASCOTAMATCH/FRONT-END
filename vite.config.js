1b00e39bec9ad2e0e9ebe7cfc8920ad982cd57d5
Erick <151527983+erick580@users.noreply.github.com>
2025-07-02 17:12:50 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/', // 👈 Asegura rutas correctas al CSS/JS en producción
   plugins: [
     vue(),
     vueDevTools(),
